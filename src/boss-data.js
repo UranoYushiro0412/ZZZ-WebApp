a7072a54d22775d609506dd6c6d7a7e406aeb0da
UranoYushiro0412 <165639099+UranoYushiro0412@users.noreply.github.com>
2026-03-30 13:58:37 +0900

    feat: add historical boss stats from periods 31 to 18

--- src/boss-data.js
+++ src/boss-data.js
@@ -40,8 +40,8 @@ export const PERIOD_DATA = {
       def: 952.8,
       stun_mult: 1.5,
       stun_time: 12,
-      stun_limit: 16647.40,
-      anomaly_limit: 3960.00,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
       weak: ["ether"],
       resist: ["ice"]
     },
@@ -51,7 +51,7 @@ export const PERIOD_DATA = {
       stun_mult: 2.0,
       stun_time: 7,
       stun_limit: 14327.95,
-      anomaly_limit: 3630.00,
+      anomaly_limit: 3630.0,
       weak: ["ice", "ether"],
       resist: []
     }
@@ -62,7 +62,7 @@ export const PERIOD_DATA = {
       def: 952.8,
       stun_mult: 1.25,
       stun_time: 12,
-      stun_limit: 16647.40,
+      stun_limit: 16647.4,
       anomaly_limit: 3960,
       weak: ["ether", "physical"],
       resist: ["fire"]
@@ -82,10 +82,426 @@ export const PERIOD_DATA = {
       def: 952.8,
       stun_mult: 1.5,
       stun_time: 12,
-      stun_limit: 16647.40,
+      stun_limit: 16647.4,
       anomaly_limit: 3630,
       weak: ["fire"],
       resist: ["electric"]
+    }
+  },
+  31: {
+    "プリマ・イコノクラスト・ヴェスポ": {
+      hp: 229600000,
+      def: 476.4,
+      stun_mult: 1.5,
+      stun_time: 15,
+      stun_limit: 19977.35,
+      anomaly_limit: 4125,
+      weak: ["ether"],
+      resist: ["ice", "fire"]
+    },
+    "冒涜者": {
+      hp: 134400000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 12,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    },
+    "ワンダリングハンター": {
+      hp: 157100000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 12,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    }
+  },
+  30: {
+    "原初の悪夢・「始まりの主」": {
+      hp: 248500000,
+      def: 476.4,
+      stun_mult: 1.5,
+      stun_time: 15,
+      stun_limit: 18236,
+      anomaly_limit: 4950,
+      weak: ["physical"],
+      resist: ["ice", "ether"]
+    },
+    "ミアズマの司祭": {
+      hp: 144400000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 12,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    },
+    "要警戒・デッドエンドブッチャ―": {
+      hp: 179100000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 12,
+      stun_limit: 15486.5,
+      anomaly_limit: 3630,
+      weak: ["ice", "ether"],
+      resist: []
+    }
+  },
+  29: {
+    "原初の悪夢・「始まりの主」": {
+      hp: 216700000,
+      def: 476.4,
+      stun_mult: 1.5,
+      stun_time: 16,
+      stun_limit: 18236,
+      anomaly_limit: 4950,
+      weak: ["physical"],
+      resist: ["ice", "ether"]
+    },
+    "未確認複合侵蝕体": {
+      hp: 131000000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16894.15,
+      anomaly_limit: 3300,
+      weak: ["electric", "ether"],
+      resist: []
+    },
+    "ワンダリングハンター": {
+      hp: 152700000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    }
+  },
+  28: {
+    "冒涜者": {
+      hp: 123100000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    },
+    "要警戒・マリオネット": {
+      hp: 145100000,
+      def: 952.8,
+      stun_mult: 2.0,
+      stun_time: 8,
+      stun_limit: 14327.95,
+      anomaly_limit: 3630.0,
+      weak: ["ice", "ether"],
+      resist: []
+    },
+    "ミアズマの司祭": {
+      hp: 127900000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    }
+  },
+  27: {
+    "原初の悪夢・「始まりの主」": {
+      hp: 216700000,
+      def: 476.4,
+      stun_mult: 1.5,
+      stun_time: 16,
+      stun_limit: 18236,
+      anomaly_limit: 4950,
+      weak: ["physical"],
+      resist: ["ice", "ether"]
+    },
+    "ミアズマ・フィーンド・名は名なれど": {
+      hp: 154300000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960,
+      weak: ["ether", "physical"],
+      resist: ["fire"]
+    },
+    "ワンダリングハンター": {
+      hp: 152700000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    }
+  },
+  26: {
+    "ワンダリングハンター": {
+      hp: 137900000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    },
+    "悪夢に縛られし者・葉釈淵": {
+      hp: 161500000,
+      def: 952.8,
+      stun_mult: 2.0,
+      stun_time: 13,
+      stun_limit: 14068.04,
+      anomaly_limit: 4356,
+      weak: ["ice", "physical"],
+      resist: ["electric"]
+    },
+    "サクリファイス・ブリンガー": {
+      hp: 170700000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3630,
+      weak: ["ice"],
+      resist: ["physical"]
+    }
+  },
+  25: {
+    "ワンダリングハンター": {
+      hp: 120400000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    },
+    "「テューポーン・デストロイヤー」": {
+      hp: 112700000,
+      def: 952.8,
+      stun_mult: 2.0,
+      stun_time: 13,
+      stun_limit: 14327.95,
+      anomaly_limit: 3630,
+      weak: ["electric"],
+      resist: ["fire"]
+    },
+    "ミアズマの司祭": {
+      hp: 114200000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    }
+  },
+  23: {
+    "ワンダリングハンター": {
+      hp: 120400000,
+      def: 1588,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16283.15,
+      anomaly_limit: 4554,
+      weak: ["ice", "fire"],
+      resist: ["physical"]
+    },
+    "ミアズマ・フィーンド・名は名なれど": {
+      hp: 134600000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960,
+      weak: ["ether", "physical"],
+      resist: ["fire"]
+    },
+    "冒涜者": {
+      hp: 109600000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    }
+  },
+  22: {
+    "ミアズマの司祭": {
+      hp: 114200000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    },
+    "冒涜者": {
+      hp: 109600000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    },
+    "要警戒・デッドエンドブッチャ―": {
+      hp: 136600000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 15486.5,
+      anomaly_limit: 3630,
+      weak: ["ice", "ether"],
+      resist: []
+    }
+  },
+  21: {
+    "ミアズマ・フィーンド・名は名なれど": {
+      hp: 121500000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960,
+      weak: ["ether", "physical"],
+      resist: ["fire"]
+    },
+    "未確認複合侵蝕体": {
+      hp: 116400000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16894.15,
+      anomaly_limit: 3300,
+      weak: ["electric", "ether"],
+      resist: []
+    },
+    "ミアズマの司祭": {
+      hp: 109600000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    }
+  },
+  20: {
+    "冒涜者": {
+      hp: 99000000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    },
+    "要警戒・デッドエンドブッチャ―": {
+      hp: 134300000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 15486.5,
+      anomaly_limit: 3630,
+      weak: ["ice", "ether"],
+      resist: []
+    },
+    "要警戒・ポンペイ": {
+      hp: 102400000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3630,
+      weak: ["fire"],
+      resist: ["electric"]
+    }
+  },
+  19: {
+    "冒涜者": {
+      hp: 99000000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 19977.35,
+      anomaly_limit: 4356,
+      weak: ["electric", "physical"],
+      resist: ["ice"]
+    },
+    "ミアズマ・フィーンド・名は名なれど": {
+      hp: 121500000,
+      def: 952.8,
+      stun_mult: 1.25,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960,
+      weak: ["ether", "physical"],
+      resist: ["fire"]
+    },
+    "ミアズマの司祭": {
+      hp: 109600000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    }
+  },
+  18: {
+    "要警戒・デッドエンドブッチャ―": {
+      hp: 118700000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 15486.5,
+      anomaly_limit: 3630,
+      weak: ["ice", "ether"],
+      resist: []
+    },
+    "ミアズマの司祭": {
+      hp: 91400000,
+      def: 952.8,
+      stun_mult: 1.5,
+      stun_time: 13,
+      stun_limit: 16647.4,
+      anomaly_limit: 3960.0,
+      weak: ["ether"],
+      resist: ["ice"]
+    },
+    "「テューポーン・デストロイヤー」": {
+      hp: 89600000,
+      def: 952.8,
+      stun_mult: 2.0,
+      stun_time: 13,
+      stun_limit: 14327.95,
+      anomaly_limit: 3630,
+      weak: ["electric"],
+      resist: ["fire"]
     }
   }
 };
